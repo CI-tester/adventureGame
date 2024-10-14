7693901b7ced68d9118f0d0f4b94952af796dd5e
olegmakeienko <molva83@gmail.com>
2024-10-15 00:00:19 +0200

    Lägg till rumsegenskaper i Room-klassen
    - Introducerat privat instansvariabel previousRoom för att spåra föregående rum.
    - Lagt till en instansvariabel accessible för att ange om rummet är tillgängligt.
    - Implementerat getter-metoder för previousRoom och accessible.

--- src/main/java/CItester/adventureGame/Room.java
+++ src/main/java/CItester/adventureGame/Room.java
@@ -1,4 +1,14 @@
 package CItester.adventureGame;
 
 public class Room {
+    private Room previousRoom;
+    private boolean accessible;
+
+    public Room getPreviousRoom() {
+        return previousRoom;
+    }
+
+    public boolean isAccessible() {
+        return accessible;
+    }
 }
