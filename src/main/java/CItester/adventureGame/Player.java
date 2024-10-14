087be71e3272797d73087a476a1e8f29540f97a9
olegmakeienko <molva83@gmail.com>
2024-10-14 23:59:50 +0200

    Implementera currentRoom i Player-klassen
    - Lagt till en privat instansvariabel currentRoom för att spåra spelarens nuvarande rum.
    - Implementerat en konstruktor som tar emot ett start rum och sätter currentRoom.
    - Lagt till metoden canMoveBack för att kontrollera om spelaren kan gå tillbaka till föregående rum.

--- src/main/java/CItester/adventureGame/Player.java
+++ src/main/java/CItester/adventureGame/Player.java
@@ -1,4 +1,14 @@
 package CItester.adventureGame;
 
 public class Player {
+    private Room currentRoom;
+
+    public Player(Room startingRoom) {
+        this.currentRoom = startingRoom;
+    }
+
+    public boolean canMoveBack() {
+        Room previousRoom = currentRoom.getPreviousRoom();
+        return previousRoom != null && previousRoom.isAccessible();
+    }
 }
