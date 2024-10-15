b5c91e68344aef573be2fdf65ddd14babde76235
Tobias <tobias199@gmail.com>
2024-10-15 10:30:57 +0200

    Added Tests For GetItems And UseItem

--- src/main/java/CItester/adventureGame/Room.java
+++ src/main/java/CItester/adventureGame/Room.java
@@ -1,4 +1,28 @@
 package CItester.adventureGame;
 
+import java.util.ArrayList;
+
 public class Room {
+String colour;
+boolean isDark;
+Temperature temprature;
+ArrayList<Item> itemsVisible;
+ArrayList<Item> itemsInvisible;
+boolean hasNextRoomKey;
+
+public Room(ArrayList<Item> itemsInvisible, ArrayList<Item> itemsVisible) {
+    this.itemsInvisible = itemsInvisible;
+    this.itemsVisible = itemsVisible;
+}
+
+public Item[] getItems(){
+    return null;
+}
+public boolean useItem(int index){
+    return false;
+}
+
+}
+enum Temperature {
+    low,medium,high
 }
