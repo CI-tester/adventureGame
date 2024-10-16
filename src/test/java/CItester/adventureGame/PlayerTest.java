3ae4905d03dbb28973bbdcd6c5bbefb8edc16031
PNorrmyr <philip.norrmyr@gmail.com>
2024-10-16 08:45:20 +0200

    Simplified lookInRoom method in Player class and modified test method.

--- src/test/java/CItester/adventureGame/PlayerTest.java
+++ src/test/java/CItester/adventureGame/PlayerTest.java
@@ -61,9 +61,9 @@ class PlayerTest {
         when(roomNullItems.getItems()).thenReturn(null);
 
 
-        assertNull(playerNullItems.lookInRoom(roomNullItems));
-        assertArrayEquals(emptyItemList, playerEmptyRoom.lookInRoom(emptyRoom));
-        assertArrayEquals(items, playerItemsInRoom.lookInRoom(roomWithItems));
+        assertNull(playerNullItems.lookInRoom());
+        assertArrayEquals(emptyItemList, playerEmptyRoom.lookInRoom());
+        assertArrayEquals(items, playerItemsInRoom.lookInRoom());
 
 
     }
